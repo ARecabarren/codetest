76f353a648436d068f5310ef0c2a387ccb898f86
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-04-28 12:11:57 -0400

    Fibo recursive array

---
+++ iterative_fibonacci.rb
@@ -0,0 +1,23 @@
+def iterative_fibo(n)
+    numbers = []
+    n.times do |time|
+        if time == 0 || time == 1
+            numbers << time
+        else
+            numbers << numbers[-2..-1].reduce {|num, total| total + num}
+        end
+    end
+    numbers
+
+end
+
+def fibs_rec(n)
+    return [0] if n == 0
+    return [0, 1] if n == 1
+  
+    array = fibs_rec(n - 1)
+    array << array[-2] + array[-1]
+end
+
+p fibs_rec(3)
+# p iterative_fibo(8)
