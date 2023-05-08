ee6a5071b9268dbb28ff0aa23645e659c131bfff
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-05-08 15:05:59 -0400

    Set basic classes

---
+++ linked_list_v2.rb
@@ -0,0 +1,11 @@
+# Class representing the full list
+class LinkedList
+    def initialize
+        
+    end
+end
+
+# Class representing list items
+def Node(value = nil, next_node = nil)
+    
+end
